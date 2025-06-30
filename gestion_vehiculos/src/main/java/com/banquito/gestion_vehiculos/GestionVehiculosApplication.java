f7c891461ca70b24c1c781acbe99c9083193aff3
saoviedo3 <78757400+saoviedo3@users.noreply.github.com>
2025-06-29 20:14:19 -0500

    Creación Proyecto

---
+++ gestion_vehiculos/src/main/java/com/banquito/gestion_vehiculos/GestionVehiculosApplication.java
@@ -0,0 +1,13 @@
+package com.banquito.gestion_vehiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionVehiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionVehiculosApplication.class, args);
+	}
+
+}
---
+++ gestion_vehiculos/src/test/java/com/banquito/gestion_vehiculos/GestionVehiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banquito.gestion_vehiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionVehiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
